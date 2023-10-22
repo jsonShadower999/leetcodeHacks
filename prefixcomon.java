45c51df2e3a681066adfe7fb56609cc06e37a9a6
jsonShadower999 <142488144+jsonShadower999@users.noreply.github.com>
2023-10-22 23:25:13 +0530

    Create prefixcomon.java

---
+++ prefixcomon.java
@@ -0,0 +1,48 @@
+
+//    public static string func1(String[] a){
+//        //if the array is empty return ""
+//        //if the array has one element return a[0]
+//        //array sort
+//        // minlen n maxlen
+//        // find the coman prefix n return subspring
+//        int size = a.Length;
+//
+//        /* if size is 0, return empty string */
+//        if (size == 0)
+//            return "";
+//
+//        if (size == 1)
+//            return a[0];
+//
+//        /* sort the array of strings */
+//        Array.Sort(a);
+//
+//        /* find the minimum length from first
+//        and last string */
+//        int end = Math.Min(a[0].Length,
+//                a[size-1].Length);
+//
+//        /* find the common prefix between the
+//        first and last string */
+//        int i = 0;
+//        while (i < end && a[0][i] == a[size-1][i] )
+//            i++;
+//
+//        string pre = a[0].Substring(0, i);
+//        return pre;
+//    }
+
+//    Input: s = "This is a java language"
+//    output: all even len string
+    public static void evenwords(String str1){
+        char[] ch1=str1.toCharArray();
+        for(char i:ch1){
+
+        }
+
+    }
+
+
+
+}
+
