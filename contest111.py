ba3ae1bac3320e3a5b44ddbe20cb986c56179868
jsonShadower999 <142488144+jsonShadower999@users.noreply.github.com>
2024-04-29 13:08:06 +0530

    Create contest111.py

---
+++ contest111.py
@@ -0,0 +1,49 @@
+# 3131. Find the Integer Added to Array I
+
+
+class Solution(object):
+    def addedInteger(self, nums1, nums2):
+        nums1.sort()
+        nums2.sort()
+        diff=nums2[0]-nums1[0]
+        return diff
+
+# You are given two arrays of equal length, nums1 and nums2.
+
+# Each element in nums1 has been increased (or decreased in the case of negative) by an integer, represented by the variable x.
+
+# As a result, nums1 becomes equal to nums2. Two arrays are considered equal when they contain the same integers with the same frequencies.
+
+# Return the integer x.
+
+ 
+
+# Example 1:
+
+# Input: nums1 = [2,6,4], nums2 = [9,7,5]
+
+# Output: 3
+
+# Explanation:
+
+# The integer added to each element of nums1 is 3.
+
+# Example 2:
+
+# Input: nums1 = [10], nums2 = [5]
+
+# Output: -5
+
+# Explanation:
+
+# The integer added to each element of nums1 is -5.
+
+# Example 3:
+
+# Input: nums1 = [1,1,1,1], nums2 = [1,1,1,1]
+
+# Output: 0
+
+# Explanation:
+
+# The integer added to each element of nums1 is 0.
